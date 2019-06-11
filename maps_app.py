7fd50a4891269cb4e6e61b8c78b268eed7f1436b
gd <gd@gds-MacBook-Pro.local>
2019-06-10 22:23:38 -0400

    Remove file

--- maps_app.py
+++ maps_app.py
@@ -6,22 +6,33 @@ import geopandas as gpd
 
 import secrets1  # .gitignore, contains API key for Census
 
+# "percent of households without internet access, you would calculate it by doing B28002_013 / B28002_001."
+# https://acsdatacommunity.prb.org/acs-data-issues/acs-basics/f/10/t/451
+
 def get_internetData(df):
     """
     Inputs: dataframe with Census tract column called 'NAMELSAD'
     Output: dataframe with Durham County, NC census data for internet usage added
     """
-    base_url=('https://api.census.gov/data/2017/acs/acs5?get=B28003_001E,NAME'
+    base_url=('https://api.census.gov/data/2017/acs/acs5?get=B28002_013E,NAME'
+    '&for=tract:*&in=state:37%20county:063&key='+secrets1.AppKey['Census'])
+    
+    base_url2=('https://api.census.gov/data/2017/acs/acs5?get=B28002_001E,NAME'
     '&for=tract:*&in=state:37%20county:063&key='+secrets1.AppKey['Census'])
  
     table2=census_query(base_url)  # Calls function below which does the heavy lifting
-    table2.rename(columns={"B28003_001E":'CmptrIntrnt'}, inplace=True)
-    table2['IntrntRate']=(table2['CmptrIntrnt'].astype(int)*100).div(df['POP'].astype(int),fill_value=1)
+    table2.rename(columns={"B28002_013E":'NoIntrnt'}, inplace=True)
+ 
+    table3=census_query(base_url2)  # Calls function below which does the heavy lifting
+    table3.rename(columns={"B28002_001E":'Households'}, inplace=True)
+ 
+    table2['IntrntRate']=(table2['NoIntrnt'].astype(int)*100).div(table3['Households'].astype(int),fill_value=1)
     table2['IntrntRate']=table2['IntrntRate'].astype(float).apply(lambda x:round(x,2))
     tables=pd.merge(table2,df, on='NAMELSAD')     
          
     return tables
 
+    
 def census_query(base_url):
     """
     Allows for different endpoints to be input for different queries to census API
@@ -38,23 +49,10 @@ def census_query(base_url):
     
     return tables
 
-def census_countyPop_getter(df): #df=pd.read_csv('pivot_table_chloro_updated.csv', header=0)
-    """
-    Input: dataframe with 'Principal County Served:' column
-    Output: Population by tract column, 'POP', merged to input df
-    """
-    base_url=('https://api.census.gov/data/2017/acs/acs5?get=B01001_001E,NAME'
-    '&for=tract:*&in=state:37%20county:063&key='+secrets1.AppKey['Census'])
-    table=census_query(base_url)
-    table.rename(columns={"B01001_001E":'POP'}, inplace=True)
-    table=get_internetData(table)
-    tables=pd.merge(table,df, on='NAMELSAD')
-
-    return tables
-
 #Import NC tracts shapefiles
 nc_tracts=gpd.read_file("zip://./Censustract_2017_37_tract.zip")
-nc_tracts= gpd.GeoDataFrame(census_countyPop_getter(nc_tracts))
+
+nc_tracts= gpd.GeoDataFrame(get_internetData(nc_tracts)) # adds columns with Data on households' internet 
 durm_tracts=nc_tracts[nc_tracts['COUNTYFP']=='063'] # restrict to Durham tracts
 durm_tracts.crs = {'init' :'epsg:4326'}
 
@@ -66,17 +64,17 @@ folium.Choropleth(
     data=durm_tracts,
     columns=['NAMELSAD','IntrntRate'],
     key_on='feature.properties.NAMELSAD',
-    fill_color='YlOrBr_r', 
+    fill_color='YlOrBr', 
     fill_opacity=0.7,
     line_opacity=0.2,
-    legend_name='Residents with internet access (%)'
+    legend_name='Durham County households without internet access (%)'
 ).add_to(m)
 
 # Use transparent tracts with tooltips so data pops up on hover
 folium.features.GeoJson(durm_tracts, name='Labels',
                style_function=lambda x: {'color':'transparent','fillColor':'transparent','weight':0},
                 tooltip=folium.features.GeoJsonTooltip(fields=['IntrntRate'],
-                                              aliases = ['Internet Access Rate (%):'],
+                                              aliases = ['Households without internet (%):'],
                                               labels=True,
                                               sticky=False
                                              )
@@ -85,6 +83,6 @@ folium.features.GeoJson(durm_tracts, name='Labels',
 folium.LayerControl().add_to(m)
 
 
-m.save("mymap003.html")
+m.save("mymap005.html")
 
 
